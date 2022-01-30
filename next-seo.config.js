a4557b8acb31162bb16e497cf1be7f2660777db3
codingwithdidem <codingwithdidem@gmail.com>
2022-01-30 19:44:40 +0300

    add seo

---
+++ next-seo.config.js
@@ -0,0 +1,47 @@
+export default {
+  defaultTitle: 'readme.so',
+  description:
+    "Use readme.so's markdown editor and templates to easily create a ReadMe for your projects",
+  additionalMetaTags: [
+    {
+      property: 'keywords',
+      content: 'readme, easiest, create, markdown, git ,github, gitlab'
+    }
+  ],
+  additionalLinkTags: [
+    {
+      rel: 'icon',
+      href: '/favicon.ico'
+    },
+    {
+      rel: 'apple-touch-icon',
+      href: '/icons/icon-96x96.png',
+      sizes: '96x96'
+    },
+    {
+      rel: 'manifest',
+      href: '/manifest.json'
+    }
+  ],
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://readme.so/',
+    title: 'readme.so',
+    site_name: 'readme.so',
+    description:
+      "Use readme.so's markdown editor and templates to easily create a ReadMe for your projects",
+    images: [
+      {
+        url: 'https://readme.so/readme.png',
+        width: 375,
+        height: 375,
+        alt: 'readme.so'
+      }
+    ]
+  },
+  twitter: {
+    handle: '@codingwithdidem',
+    cardType: 'summary'
+  }
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import { appWithTranslation } from 'next-i18next'
+import NextNProgress from 'nextjs-progressbar'
+import { DefaultSeo } from 'next-seo'
+import SEO from 'next-seo.config'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <NextNProgress height={3} color={'#ffafccff'} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default appWithTranslation(MyApp)
