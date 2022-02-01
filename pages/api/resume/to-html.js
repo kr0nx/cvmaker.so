f9310dfe733948ae3f1a5a186a5966c93235f4c2
codingwithdidem <codingwithdidem@gmail.com>
2022-02-02 00:46:05 +0300

    saldokasodk

--- pages/api/resume/to-html.js
+++ pages/api/resume/to-html.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import getConfig from 'next/config'
 import nodePandoc from 'node-pandoc'
+const { spawn } = require('child_process')
 
 const serverPath = staticFilePath => {
   return path.join(getConfig().serverRuntimeConfig.PROJECT_ROOT, staticFilePath)
@@ -8,6 +9,7 @@ const serverPath = staticFilePath => {
 
 export default async (req, res) => {
   if (req.method === 'POST') {
+    spawn('node', ['script.js'])
     const { markdown } = req.body
 
     // const cssPath = serverPath('public/resume-css-stylesheet.css')
