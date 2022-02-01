8f1b7ef07ccd77d5ecc412651c2e1b1071c88c99
codingwithdidem <codingwithdidem@gmail.com>
2022-02-02 00:31:49 +0300

    donee

--- pages/api/resume/to-html.js
+++ pages/api/resume/to-html.js
@@ -14,7 +14,7 @@ export default async (req, res) => {
 
     print(cssPath)
 
-    let args = `-s --toc -H ${cssPath}`
+    let args = `-s --toc `
 
     nodePandoc(markdown, args, (err, result) => {
       if (err) {
