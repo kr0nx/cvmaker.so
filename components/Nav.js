f4235a6e5d1dac90fc4a7ed290c69291b5e1ad0d
codingwithdidem <codingwithdidem@gmail.com>
2022-02-02 22:28:30 +0300

    fix errors

--- components/Nav.js
+++ components/Nav.js
@@ -45,28 +45,28 @@ const Nav = () => {
       </Link>
 
       <div>
-        <div class="p-4">
-          <div class="group relative">
-            <button class="border-2 border-baby-blue-eyes font-semibold uppercase text-baby-blue-eyes text-sm px-6 py-2 rounded-md">
+        <div className="p-4">
+          <div className="group relative">
+            <button className="border-2 border-baby-blue-eyes font-semibold uppercase text-baby-blue-eyes text-sm px-6 py-2 rounded-md">
               Download
             </button>
             <nav
-              tabindex="0"
-              class="overflow-hidden border-2 bg-gray-100 invisible border-gray-800 rounded-md w-full absolute left-0 top-full transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1"
+              tabIndex="0"
+              className="overflow-hidden border-2 bg-gray-100 invisible border-gray-800 rounded-md w-full absolute left-0 top-full transition-all opacity-0 group-focus-within:visible group-focus-within:opacity-100 group-focus-within:translate-y-1"
             >
               <ul>
                 <li>
-                  <a href="#" class="block px-4 py-1 hover:bg-gray-200 " onClick={downloadHtml}>
+                  <a href="#" className="block px-4 py-1 hover:bg-gray-200 " onClick={downloadHtml}>
                     HTML5
                   </a>
                 </li>
                 <li>
-                  <a href="#" class="block px-4 py-1 hover:bg-gray-200">
+                  <a href="#" className="block px-4 py-1 hover:bg-gray-200">
                     PDF
                   </a>
                 </li>
                 <li>
-                  <a href="#" class="block px-4 py-1 hover:bg-gray-200">
+                  <a href="#" className="block px-4 py-1 hover:bg-gray-200">
                     DOCX
                   </a>
                 </li>
