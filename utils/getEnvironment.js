b2264d37a45f2ec090d645a4b4c3d851a1fde4b6
codingwithdidem <codingwithdidem@gmail.com>
2022-02-08 01:38:43 +0300

    updated

--- utils/getEnvironment.js
+++ utils/getEnvironment.js
@@ -3,7 +3,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 const getEnvironment = () => {
   let baseUrl = 'localhost:3000'
 
-  baseUrl = isProduction ? 'https://cvmaker-so.vercel.app' : 'http://localhost:3000'
+  baseUrl = isProduction ? 'https://cv-builder-steel.vercel.app' : 'http://localhost:3000'
 
   return {
     baseUrl,
